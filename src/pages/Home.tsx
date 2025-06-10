1efc97d25c992e04b324056cf90a7d875c47e6a1
Fraser Alan Dunn <fraseralan.dunn@dxc.com>
2025-06-10 21:48:33 +0100

    kinda fixed lmao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,11 +10,11 @@ const Home = () => {
           <HeaderBar />
         </div>
 
-        <div className="h-[70%] col-start-1 col-end-2 row-start-2 row-end-2 text-center border-r-1 border-muted flex items-center justify-center">
+        <div className="h-[92vh] col-start-1 col-end-2 row-start-2 row-end-2 text-center border-r-1 border-muted flex items-center justify-center relative">
           <div className="h-11/12 w-11/12 border-1 border-muted flex items-center justify-center"></div>
         </div>
 
-        <div className="h-[70%] col-start-2 col-end-3 row-start-2 row-end-2 text-center border-r-1 border-muted flex items-center justify-center">
+        <div className="h-[92vh] col-start-2 col-end-3 row-start-2 row-end-2 text-center border-r-1 border-muted flex items-center justify-center">
           <div className="h-11/12 w-11/12 border-1 border-muted flex items-center justify-center">
             <ScrollArea className="h-full w-full rounded-md bg-[#171717] p-4">
               <h1 className="text-3xl mb-8">Onboarding Timeline</h1>
